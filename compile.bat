948ffbf4452a30634b172b8d485cc703454f7bbf
endelf <93210351+endelf@users.noreply.github.com>
2024-09-18 13:09:47 +0200

    Update compile.bat
    Création automatique du dossier build

--- compile.bat
+++ compile.bat
@@ -1,3 +1,4 @@
+mkdir build
 gcc -I../include -c src/http.c -o build/http.o && gcc -I../include -c src/main.c -o build/main.o
 gcc build/http.o build/main.o -o build/main -lcurl
 del "build\\main.o"
